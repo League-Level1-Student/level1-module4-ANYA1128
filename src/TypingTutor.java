88e68c4c64dda8cd6baa0f082f9f22692f39084b
league <league@imac-26.attlocal.net>
2019-05-31 18:26:16 -0700

    TypingTutor, BookOfIllusions(IP) finish if statements=optional part

--- src/TypingTutor.java
+++ src/TypingTutor.java
@@ -1,7 +1,7 @@
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.sql.Date;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JFrame;
@@ -9,80 +9,85 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+import com.sun.javafx.scene.EnteredExitedHandler;
+
 public class TypingTutor implements KeyListener {
-JFrame jeff = new JFrame("Type or Die");
-JPanel jp = new JPanel();
-JLabel label = new JLabel();
-char currentLetter;
-Date timeAtStart = new Date(currentLetter);
+	JFrame jeff = new JFrame("Type or Die");
+	JPanel jp = new JPanel();
+	JLabel label = new JLabel();
+	char currentLetter;
+	Date timeAtStart = new Date();
+	int numberOfCorrectCharactersTyped = 0;
+	int numberOfIncorrectCharactersTyped = 0;
 
-public static void main(String[] args) {
-	TypingTutor idk = new TypingTutor();
-	idk.setup();
-}
-public void setup() {
-	currentLetter = generateRandomLetter();
-	jeff.setVisible(true);
-	jeff.setSize(200, 200);
-	jeff.addKeyListener(this);
-	label.setText(currentLetter+"");
-	label.setFont(label.getFont().deriveFont(180.0f));
-	label.setHorizontalAlignment(JLabel.CENTER);
-	jp.add(label);
-	jeff.add(jp);
-	jeff.pack();
-	
-	showTypingSpeed(currentLetter);
-	
-
-}
-private void showTypingSpeed(int numberOfCorrectCharactersTyped) {
-    Date timeAtEnd = new Date(numberOfCorrectCharactersTyped);
-    long gameDuration = timeAtEnd.getTime() - timeAtStart.getTime();
-    long gameInSeconds = (gameDuration / 1000) % 60;
-    double charactersPerSecond = ((double) numberOfCorrectCharactersTyped / (double) gameInSeconds);
-    int charactersPerMinute = (int) (charactersPerSecond * 60);
-    if(gameInSeconds>6) {
-    	   System.out.println("Your typing speed is " + charactersPerMinute + " characters per minute.");
-    
-}
-}
-
-
-
-
-char generateRandomLetter() {
-      Random r = new Random();    
-      return (char) (r.nextInt(26) + 'a');
-}
-@Override
-public void keyTyped(KeyEvent e) {
-	// TODO Auto-generated method stub
-		if(e.getKeyChar()==currentLetter) {
-		System.out.println("correct");
-		jp.setBackground(Color.GREEN);
-	
-
+	public static void main(String[] args) {
+		TypingTutor idk = new TypingTutor();
+		idk.setup();
 	}
-	else {
-	
-		jp.setBackground(Color.RED);
-	}
+
+	public void setup() {
 		currentLetter = generateRandomLetter();
-		label.setText(currentLetter+"");
+		jeff.setVisible(true);
+		jeff.setSize(200, 200);
+		jeff.addKeyListener(this);
+		label.setText(currentLetter + "");
+		label.setFont(label.getFont().deriveFont(180.0f));
+		label.setHorizontalAlignment(JLabel.CENTER);
+		jp.add(label);
+		jeff.add(jp);
+		jeff.pack();
 
-}
-@Override
-public void keyPressed(KeyEvent e) {
-	// TODO Auto-generated method stub
-	System.out.println("you typed: "+e.getKeyChar());
+	}
+
+	private void showTypingSpeed(int numberOfCorrectCharactersTyped) {
+		Date timeAtEnd = new Date();
+		long gameDuration = timeAtEnd.getTime() - timeAtStart.getTime();
+		long gameInSeconds = (gameDuration / 1000);
+		double charactersPerSecond = ((double) numberOfCorrectCharactersTyped / (double) gameInSeconds);
+		int charactersPerMinute = (int) (charactersPerSecond * 60);
+		// if(gameInSeconds>6) {
+		System.out.println("Your typing speed is " + charactersPerMinute + " characters per minute.");
+
+//}
+	}
+
+	char generateRandomLetter() {
+		Random r = new Random();
+		return (char) (r.nextInt(26) + 'a');
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getKeyChar() == currentLetter) {
+			System.out.println("correct");
+			jp.setBackground(Color.GREEN);
+			numberOfCorrectCharactersTyped += 1;
+
+		} else {
+
+			jp.setBackground(Color.RED);
+			numberOfIncorrectCharactersTyped += 1;
+		}
+		currentLetter = generateRandomLetter();
+		label.setText(currentLetter + "");
 	
-}
-@Override
-public void keyReleased(KeyEvent e) {
-	// TODO Auto-generated method stub
-	
-}
-}
 
+	}
 
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("you typed: " + e.getKeyChar());
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if (numberOfCorrectCharactersTyped + numberOfIncorrectCharactersTyped >= 10) {
+			showTypingSpeed(numberOfCorrectCharactersTyped);
+		}
+
+	}
+}
--- src/book_of_illusions/BookOfIllusions.java
+++ src/book_of_illusions/BookOfIllusions.java
@@ -11,6 +11,7 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
  
 /** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
@@ -18,21 +19,35 @@ import javax.swing.JLabel;
 public class BookOfIllusions extends MouseAdapter {
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
+JFrame wow = new JFrame();
 
 	public void run() {
 		// 2. make the frame visible
+		wow.setVisible(true);
 		// 3. set the size of the frame
+		wow.setSize(1000, 1000);
 		// 4. find 2 images and save them to your project’s default package
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+		String illusion1 = "thatone.jpg";
+		String illusion2 = "ladyillusion.jpg";
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+		JLabel that = new JLabel(illusion1);
+		JLabel heh = new JLabel(illusion2);
+		wow.add(that);
+		wow.add(heh);
 		// 7. use the "loadImage..." methods below to initialize your JLabel
 		// 8. add your JLabel to the frame
 		// 9. call the pack() method on the frame
+		wow.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		wow.addMouseListener(this);
 	}
 
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		if() {
+			
+		}
 		// 12. remove everything from the frame that was added earlier
 		// 13. load a new image like before (this is more than one line of code)
 		// 14. pack the frame
